3739e244bfe5e93c779f70a1abfd283c0630c4c1
Daniel <davilareko@gmail.com>
2023-01-26 23:29:09 +0100

    Add custom file path

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "stb_image.h"
 #include <iostream>
 #include <vector>
+#include <filesystem>
 
 std::vector<std::string> compareImages(std::string filename1, std::string filename2)
 {
@@ -76,7 +77,17 @@ std::vector<std::string> compareImages(std::string filename1, std::string filena
 
 int main()
 {
-    auto out = compareImages("test.png", "test3.png");
+    std::string filename1;
+    std::string filename2;
+    std::cin >> filename1;
+    std::cin >> filename2;
+
+    if (!std::filesystem::exists(filename1))
+        return 1;
+    if (!std::filesystem::exists(filename2))
+        return 1;
+
+    auto out = compareImages(filename1, filename2);
     std::cout << "Result:\n";
     std::cout << "Different pixels: " << out[0] << " \n";
     std::cout << "That's " << out[1] << "\% different image.\n";
