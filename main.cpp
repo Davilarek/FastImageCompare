5b260cf517ace4b19ebe7dca31acc34da11fb696
Daniel <davilareko@gmail.com>
2023-01-27 02:57:25 +0100

    Add arguments support

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <filesystem>
 #include "compare_pixels.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    std::vector<std::string> args;
+    for (int i = 1; i < argc; i++)
+    {
+        args.push_back(argv[i]);
+    }
+
     std::string filename1;
     std::string filename2;
-    std::cout << "Source filename: ";
-    std::cin >> filename1;
-    std::cout << "Compare filename: ";
-    std::cin >> filename2;
+    if (args.size() == 0)
+    {
+        std::cout << "Source filename: ";
+        std::cin >> filename1;
+        std::cout << "Compare filename: ";
+        std::cin >> filename2;
+    }
+    else
+    {
+        // filename1 = args[0] + "";
+        // filename2 = args[1] + "";
+        filename1 = argv[1];
+        filename2 = argv[2];
+    }
 
     if (!std::filesystem::exists(filename1))
         return 1;
@@ -34,7 +50,10 @@ int main()
         return 0;
     }
 
-    auto out = compareImages(filename1, filename2, true);
+    auto filename1Normal = std::filesystem::canonical(filename1);
+    auto filename2Normal = std::filesystem::canonical(filename2);
+
+    auto out = compareImages(filename1Normal.string(), filename2Normal.string(), true);
     std::cout << "Result:\n";
     std::cout << "Different pixels: " << out[0] << " \n";
     std::cout << "That's " << out[1] << "\% different image.\n";
